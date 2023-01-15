0f38019c9c10de51eaa86ec6e19bc47763a82031
5late <christianrfernandes5@gmail.com>
2023-01-14 22:03:35 -0500

    updated

--- btlb/index.js
+++ btlb/index.js
@@ -1,5 +1,7 @@
 file = './bars.txt'
 
+let used = []
+
 function randomLyric() {
 fetch(file).then(response => response.text()).then(data => {
 	let lyrics = data.split("\n")
@@ -11,9 +13,19 @@ fetch(file).then(response => response.text()).then(data => {
     let text_line_split = text_line.split('"')
 	let text_line_split_who_song = text_line_split[2].split('-')
 
-	document.getElementById('lyric').innerText = `"${text_line_split[1]}"`
-	document.getElementById('who').innerText = text_line_split_who_song[1]
-	document.getElementById('song').innerText = text_line_split_who_song[2]
+	console.log(lyrics.length, used.length)
+	if (used.length == lyrics.length -1){
+		used.length = 0
+	}
+	if (!used.includes(text_line_split[1])){
+		used.push(text_line_split[1])
+		document.getElementById('lyric').innerText = `"${text_line_split[1]}"`
+		document.getElementById('who').innerText = text_line_split_who_song[1]
+		document.getElementById('song').innerText = text_line_split_who_song[2]
+	} else {
+		randomLyric()
+	}
+
 })
 }
 
